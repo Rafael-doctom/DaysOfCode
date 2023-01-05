8f4712787467e2fde7fffd033d2cb596f6eaaa5d
Rafael-doctom <dasilva.rafaelpt@gmail.com>
2023-01-05 14:57:30 -0300

    exercise found myName

---
+++ month-01/day02/myName.js
@@ -0,0 +1,10 @@
+let myName = 'My name is Rafael';
+let cut = myName.split(' ').indexOf('Rafael');
+
+if (cut) {
+    console.log(`Encontrei my name at position ${cut + 1}`);
+} else {
+    console.log(`I can't find Nemo `);
+};
+
+
